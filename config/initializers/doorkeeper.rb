078520995956ff49466ad40e822ff1f3b152c341
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2018-02-13 12:10:24 +0100

    Change heroku base controller

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -42,7 +42,7 @@ Doorkeeper.configure do
   # The controller Doorkeeper::ApplicationController inherits from.
   # Defaults to ActionController::Base.
   # https://github.com/doorkeeper-gem/doorkeeper#custom-base-controller
-  base_controller 'ActionController::API'
+  # base_controller 'ActionController::API'
 
   # Reuse access token for the same resource owner within an application (disabled by default)
   # Rationale: https://github.com/doorkeeper-gem/doorkeeper/issues/383
