9f06fcec9ba28b66e78e5d0279eeb20b56560869
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2018-02-13 12:36:14 +0100

    Override doorkeeper application controller

---
+++ app/controllers/doorkeeper/application_controller.rb
@@ -0,0 +1,10 @@
+# HACK: doorkeeper does not support Rails API at this moment
+# It is necessary to remove protect_from_forgery so it can work in production
+module Doorkeeper
+  class ApplicationController < Doorkeeper.configuration.base_controller.constantize
+    # include Helpers::Controller
+
+    # protect_from_forgery with: :exception
+    # helper 'doorkeeper/dashboard'
+  end
+end
