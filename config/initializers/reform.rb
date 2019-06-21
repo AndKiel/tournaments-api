cf02c019498c2225d973d52c1b2d971d25035a91
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2019-06-21 15:29:05 +0200

    Remove wrap_parameters initializer

--- config/initializers/reform.rb
+++ config/initializers/reform.rb
@@ -4,5 +4,6 @@ require 'reform/form/active_model/validations'
 require 'reform/form/validation/unique_validator'
 
 Reform::Form.class_eval do
+  # TODO: switch to dry-validation?
   feature Reform::Form::ActiveModel::Validations
 end
