daa37a424f3153dd6895b328f57888774832dbcf
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-24 15:27:48 +0200

    Fix player and user forms

--- app/forms/player_form.rb
+++ app/forms/player_form.rb
@@ -7,14 +7,15 @@ class PlayerForm < Reform::Form
            }
 
   validation do
+    option :form
+
     params do
       required(:result_values).filled(:array?)
     end
 
-    # FIXME: How to use form/model from dry?
-    # rule(:result_values) do
-    #   count = form.model.tournament.result_names.length
-    #   key.failure(:invalid) unless result_values.length == count
-    # end
+    rule(:result_values) do
+      count = form.model.tournament.result_names.length
+      key.failure(I18n.t('errors.attributes.result_values.invalid')) unless value.length == count
+    end
   end
 end
--- app/forms/user/base_form.rb
+++ app/forms/user/base_form.rb
@@ -7,14 +7,15 @@ class User < ApplicationRecord
     property :password_confirmation, virtual: true
 
     validation do
+      option :form
+
       params do
         required(:email).filled(:str?)
       end
 
-      # FIXME: How to access form/model from dry?
       rule(:email) do
         key.failure(I18n.t('errors.messages.invalid_email')) if ValidatesEmailFormatOf.validate_email_format(value)
-        # key.failure(:taken) unless User.where.not(id: form.model.id).find_by(email: value).nil?
+        key.failure(I18n.t('errors.messages.taken')) unless User.where.not(id: form.model.id).find_by(email: value).nil?
       end
     end
   end
