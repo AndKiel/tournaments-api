39365df0eb8a75f703b686be59784a870921f492
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-25 19:38:25 +0200

    Add password comment

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user.#{n}@mail.co" }
+    # password
     password_digest { '$2a$10$DduaGQK4NkMwtaa..reSv.7IruCI35cKIBodTuOPDDb/yPstoqmH2' }
   end
 end
