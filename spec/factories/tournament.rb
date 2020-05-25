caf62d8e68598051d030d129b1b39c033c71f898
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-25 19:42:00 +0200

    Add traits to tournament

--- spec/factories/tournament.rb
+++ spec/factories/tournament.rb
@@ -7,5 +7,15 @@ FactoryBot.define do
     result_names { ['Win'] }
     starts_at { 1.day.since }
     status { 'created' }
+
+    trait :in_progress do
+      starts_at { 1.minute.ago }
+      status { 'in_progress' }
+    end
+
+    trait :ended do
+      starts_at { 1.day.ago }
+      status { 'ended' }
+    end
   end
 end
