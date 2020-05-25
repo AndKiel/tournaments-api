10bd5dfefb81c9913d2715efc81ce083c10023f5
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-25 19:31:13 +0200

    Defnine tournament factory

--- spec/factories/tournament.rb
+++ spec/factories/tournament.rb
@@ -2,6 +2,10 @@
 
 FactoryBot.define do
   factory :tournament do
-
+    competitors_limit { 12 }
+    name { 'Game Tournament' }
+    result_names { ['Win'] }
+    starts_at { 1.day.since }
+    status { 'created' }
   end
 end
