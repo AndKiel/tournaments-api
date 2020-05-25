a728ba0e81b83471dbcd945e543264c7cb5207ce
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-25 19:48:25 +0200

    Add traits to competitor

--- spec/factories/competitor.rb
+++ spec/factories/competitor.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
   factory :competitor do
     sequence(:name) { |n| "Competitor #{n}" }
     status { 'enlisted' }
+
+    trait :confirmed do
+      status { 'confirmed' }
+    end
   end
 end
