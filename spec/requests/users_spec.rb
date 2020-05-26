9dd431496de95e8368ae4dbc8beae84bb671e36c
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-26 19:58:31 +0200

    Update users spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe 'Users', type: :request do
   end
 
   context 'when authenticated' do
-    authenticate(:john)
+    auth
 
     describe 'GET #show' do
       it 'returns authenticated User' do
