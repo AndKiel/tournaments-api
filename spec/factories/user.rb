e08ab146b1428659e4c9ddef5b62ecd401709026
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-26 17:15:37 +0200

    Add aliases to user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :user do
+  factory :user, aliases: [:organiser, :resource_owner] do
     sequence(:email) { |n| "user.#{n}@mail.co" }
     # password
     password_digest { '$2a$10$DduaGQK4NkMwtaa..reSv.7IruCI35cKIBodTuOPDDb/yPstoqmH2' }
