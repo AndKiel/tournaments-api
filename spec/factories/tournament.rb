b52cc545c466bd6f026900fd48751f2eb8bc6bcb
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-26 19:59:12 +0200

    Update tournament factory

--- spec/factories/tournament.rb
+++ spec/factories/tournament.rb
@@ -9,19 +9,23 @@ FactoryBot.define do
     competitors_limit { 12 }
     name { 'Game Tournament' }
     result_names { Array.new(result_names_count) { 'Win' } }
-    starts_at { 1.day.since }
+    starts_at { 1.minute.ago }
     status { 'created' }
 
     association :organiser
+
+    trait :ended do
+      starts_at { 1.day.ago }
+      status { 'ended' }
+    end
 
     trait :in_progress do
       starts_at { 1.minute.ago }
       status { 'in_progress' }
     end
 
-    trait :ended do
-      starts_at { 1.day.ago }
-      status { 'ended' }
+    trait :upcoming do
+      starts_at { 1.day.since }
     end
   end
 end
