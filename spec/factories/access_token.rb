11f8804f5bbcf8265fd78dcccd39a2edd3141037
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-26 19:57:30 +0200

    Update oauth request spec

--- spec/factories/access_token.rb
+++ spec/factories/access_token.rb
@@ -4,7 +4,5 @@ FactoryBot.define do
   factory :access_token, class: 'Doorkeeper::AccessToken' do
     expires_in { 2.hours }
     scopes { 'public' }
-
-    association :resource_owner
   end
 end
--- spec/support/request_macros.rb
+++ spec/support/request_macros.rb
@@ -5,13 +5,19 @@ module RequestMacros
 
   def authenticate(user_sym)
     let(:current_user) { users(user_sym) }
-    let!(:access_token) do
+    let(:access_token) do
       Doorkeeper::AccessToken.create!(
         expires_in: 100,
         resource_owner_id: current_user.id,
         scopes: 'public'
       )
     end
-    let!(:auth_headers) { { 'Authorization': "Bearer #{access_token.token}" } }
+    let(:auth_headers) { { 'Authorization': "Bearer #{access_token.token}" } }
+  end
+
+  def auth
+    let(:current_user) { create(:user) }
+    let(:access_token) { create(:access_token, resource_owner_id: current_user.id) }
+    let(:auth_headers) { { 'Authorization': "Bearer #{access_token.token}" } }
   end
 end
