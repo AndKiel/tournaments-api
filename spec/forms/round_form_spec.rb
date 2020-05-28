ee191ae7c6a52eeb6341ac099673237baced2284
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-28 19:56:45 +0200

    Fix Rspec/NamedSubject

--- spec/forms/round_form_spec.rb
+++ spec/forms/round_form_spec.rb
@@ -5,42 +5,42 @@ require 'rails_helper'
 RSpec.describe RoundForm do
   let(:round) { build(:round) }
 
-  subject { described_class.new(round) }
+  subject(:form) { described_class.new(round) }
 
   it 'validates presence of competitors limit' do
-    result = subject.validate(competitors_limit: nil)
+    result = form.validate(competitors_limit: nil)
     expect(result).to be false
-    expect(subject.errors[:competitors_limit]).to include I18n.t('errors.messages.blank')
+    expect(form.errors[:competitors_limit]).to include I18n.t('errors.messages.blank')
   end
 
   it 'validates numericality of competitors limit' do
-    result = subject.validate(competitors_limit: 2.5)
+    result = form.validate(competitors_limit: 2.5)
     expect(result).to be false
-    expect(subject.errors[:competitors_limit]).to include I18n.t('errors.messages.not_an_integer')
+    expect(form.errors[:competitors_limit]).to include I18n.t('errors.messages.not_an_integer')
 
-    result = subject.validate(competitors_limit: -20)
+    result = form.validate(competitors_limit: -20)
     expect(result).to be false
-    expect(subject.errors[:competitors_limit]).to include I18n.t('errors.messages.greater_than', count: 1)
+    expect(form.errors[:competitors_limit]).to include I18n.t('errors.messages.greater_than', count: 1)
   end
 
   it 'validates presence of tables count' do
-    result = subject.validate(tables_count: nil)
+    result = form.validate(tables_count: nil)
     expect(result).to be false
-    expect(subject.errors[:tables_count]).to include I18n.t('errors.messages.blank')
+    expect(form.errors[:tables_count]).to include I18n.t('errors.messages.blank')
   end
 
   it 'validates numericality of tables count' do
-    result = subject.validate(tables_count: 2.5)
+    result = form.validate(tables_count: 2.5)
     expect(result).to be false
-    expect(subject.errors[:tables_count]).to include I18n.t('errors.messages.not_an_integer')
+    expect(form.errors[:tables_count]).to include I18n.t('errors.messages.not_an_integer')
 
-    result = subject.validate(tables_count: -20)
+    result = form.validate(tables_count: -20)
     expect(result).to be false
-    expect(subject.errors[:tables_count]).to include I18n.t('errors.messages.greater_than', count: 0)
+    expect(form.errors[:tables_count]).to include I18n.t('errors.messages.greater_than', count: 0)
   end
 
   it 'returns true for valid attributes' do
-    result = subject.validate(
+    result = form.validate(
       competitors_limit: 8,
       tables_count: 4
     )
--- spec/forms/tournament_form_spec.rb
+++ spec/forms/tournament_form_spec.rb
@@ -5,60 +5,60 @@ require 'rails_helper'
 RSpec.describe TournamentForm do
   let(:tournament) { build(:tournament) }
 
-  subject { described_class.new(tournament) }
+  subject(:form) { described_class.new(tournament) }
 
   it 'validates presence of competitors limit' do
-    result = subject.validate(competitors_limit: nil)
+    result = form.validate(competitors_limit: nil)
     expect(result).to be false
-    expect(subject.errors[:competitors_limit]).to include I18n.t('errors.messages.blank')
+    expect(form.errors[:competitors_limit]).to include I18n.t('errors.messages.blank')
   end
 
   it 'validates numericality of competitors limit' do
-    result = subject.validate(competitors_limit: 2.5)
+    result = form.validate(competitors_limit: 2.5)
     expect(result).to be false
-    expect(subject.errors[:competitors_limit]).to include I18n.t('errors.messages.not_an_integer')
+    expect(form.errors[:competitors_limit]).to include I18n.t('errors.messages.not_an_integer')
 
-    result = subject.validate(competitors_limit: -20)
+    result = form.validate(competitors_limit: -20)
     expect(result).to be false
-    expect(subject.errors[:competitors_limit]).to include I18n.t('errors.messages.greater_than', count: 1)
+    expect(form.errors[:competitors_limit]).to include I18n.t('errors.messages.greater_than', count: 1)
   end
 
   it 'validates presence of name' do
-    result = subject.validate(name: '')
+    result = form.validate(name: '')
     expect(result).to be false
-    expect(subject.errors[:name]).to include I18n.t('errors.messages.blank')
+    expect(form.errors[:name]).to include I18n.t('errors.messages.blank')
   end
 
   it 'validates presence of result names' do
-    result = subject.validate(result_names: nil)
+    result = form.validate(result_names: nil)
     expect(result).to be false
-    expect(subject.errors[:result_names]).to include I18n.t('errors.messages.blank')
+    expect(form.errors[:result_names]).to include I18n.t('errors.messages.blank')
   end
 
   it 'validates length of result names' do
-    result = subject.validate(result_names: [])
+    result = form.validate(result_names: [])
     expect(result).to be false
-    expect(subject.errors[:result_names]).to include I18n.t('errors.messages.too_short', count: 1)
+    expect(form.errors[:result_names]).to include I18n.t('errors.messages.too_short', count: 1)
   end
 
   it 'validates presence of starts at' do
-    result = subject.validate(starts_at: nil)
+    result = form.validate(starts_at: nil)
     expect(result).to be false
-    expect(subject.errors[:starts_at]).to include I18n.t('errors.messages.blank')
+    expect(form.errors[:starts_at]).to include I18n.t('errors.messages.blank')
   end
 
   it 'validates timeliness of starts at' do
-    result = subject.validate(starts_at: 'not.a.date')
+    result = form.validate(starts_at: 'not.a.date')
     expect(result).to be false
-    expect(subject.errors[:starts_at]).to include I18n.t('errors.messages.invalid_datetime')
+    expect(form.errors[:starts_at]).to include I18n.t('errors.messages.invalid_datetime')
 
-    result = subject.validate(starts_at: 1.day.ago)
+    result = form.validate(starts_at: 1.day.ago)
     expect(result).to be false
-    expect(subject.errors[:starts_at]).to include I18n.t('errors.messages.future_date')
+    expect(form.errors[:starts_at]).to include I18n.t('errors.messages.future_date')
   end
 
   it 'returns true for valid attributes' do
-    result = subject.validate(
+    result = form.validate(
       competitors_limit: 8,
       name: 'Tenkaichi Budokai',
       result_names: ['Win'],
