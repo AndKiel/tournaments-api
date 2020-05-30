c84a4b49a4ffde9bd6642ae2a6eb209107b845da
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-30 18:30:24 +0200

    chore: Update schemacrawler (#24)

--- config/initializers/reform.rb
+++ config/initializers/reform.rb
@@ -4,6 +4,5 @@ require 'reform/form/active_model/validations'
 require 'reform/form/validation/unique_validator'
 
 Reform::Form.class_eval do
-  # TODO: switch to dry-validation?
   feature Reform::Form::ActiveModel::Validations
 end
