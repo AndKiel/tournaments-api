afe55b488856623c29d1b99a5755c256900ad520
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-06-01 17:34:42 +0200

    Rewrite competitor form

--- app/forms/competitor_form.rb
+++ app/forms/competitor_form.rb
@@ -4,7 +4,20 @@ class CompetitorForm < Reform::Form
   property :name
   property :tournament_id, writeable: false
 
-  validates :name,
-            presence: true,
-            unique: { scope: [:tournament_id] }
+  validation do
+    option :form
+
+    params do
+      required(:name).filled(:str?)
+    end
+
+    rule(:name) do
+      if Competitor
+           .where(name: value, tournament_id: form.model.tournament_id)
+           .where.not(id: form.model.id)
+           .exists?
+        key.failure(I18n.t('errors.messages.taken'))
+      end
+    end
+  end
 end
