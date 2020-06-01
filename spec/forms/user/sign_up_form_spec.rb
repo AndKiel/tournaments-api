96ac69602931db54688c750917fa16d7e1ce2250
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-06-01 18:47:13 +0200

    Fix sign up spec

--- spec/forms/user/sign_up_form_spec.rb
+++ spec/forms/user/sign_up_form_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe User::SignUpForm do
   it 'validates presence of email' do
     result = form.validate(email: '')
     expect(result).to be false
-    expect(form.errors[:email]).to include I18n.t('errors.messages.blank')
+    expect(form.errors[:email]).to include 'must be filled'
   end
 
   it 'validates format of email' do
     result = form.validate(email: 'not.an.email')
     expect(result).to be false
-    expect(form.errors[:email]).to include I18n.t('activemodel.errors.messages.invalid_email_address')
+    expect(form.errors[:email]).to include I18n.t('errors.messages.invalid_email')
   end
 
   it 'validates uniqueness of email' do
@@ -29,7 +29,7 @@ RSpec.describe User::SignUpForm do
   it 'validates presence of password' do
     result = form.validate(password: '')
     expect(result).to be false
-    expect(form.errors[:password]).to include I18n.t('errors.messages.blank')
+    expect(form.errors[:password]).to include 'must be filled'
   end
 
   it 'validates confirmation of password' do
@@ -38,7 +38,7 @@ RSpec.describe User::SignUpForm do
       password_confirmation: 'orNot'
     )
     expect(result).to be false
-    expect(form.errors[:password_confirmation]).to include I18n.t('errors.messages.confirmation', attribute: 'Password')
+    expect(form.errors[:password_confirmation]).to include I18n.t('errors.messages.password_mismatch')
   end
 
   it 'returns true for valid attributes' do
