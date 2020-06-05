70e7cc06a4ef93f5402d6d8bfc812de54d8050be
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-06-05 08:42:14 +0200

    Test tournament create contract

--- app/contracts/tournament/create_contract.rb
+++ app/contracts/tournament/create_contract.rb
@@ -3,14 +3,15 @@
 class Tournament < ApplicationRecord
   class CreateContract < ApplicationContract
     json do
-      required(:competitors_limit).filled(:int?, gt?: 1)
-      required(:name).filled(:str?)
-      required(:result_names).filled(:array?).each(:str?, :filled?)
+      required(:competitors_limit).value(:int?, gt?: 1)
+      optional(:description).value(:str?)
+      required(:name).value(:str?, :filled?)
+      required(:result_names).value(:array?, :filled?).each(:str?, :filled?)
       required(:starts_at).value(:date_time)
     end
 
     rule(:starts_at) do
-      key.failure(:after_now?) unless value > Time.current
+      key.failure(:after_now?) if value <= Time.current
     end
   end
 end
