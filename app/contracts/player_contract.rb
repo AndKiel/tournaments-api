4396dc2a64343d9408e1a4b8f70e653e7e2ed5c6
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-06-06 10:17:38 +0200

    Test player contract

--- app/contracts/player_contract.rb
+++ app/contracts/player_contract.rb
@@ -4,11 +4,11 @@ class PlayerContract < ApplicationContract
   option :model
 
   json do
-    required(:result_values).array(:int?)
+    required(:result_values).value(:array?, :filled?).each(:int?)
   end
 
   rule(:result_values) do
     result_names_count = model.tournament.result_names.length
-    key.failure(I18n.t('errors.attributes.result_values.invalid')) if value.length != result_names_count
+    key.failure(I18n.t('dry_validation.errors.rules.result_values.size?')) if value.length != result_names_count
   end
 end
